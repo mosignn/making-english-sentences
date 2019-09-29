8f91dbe148bcfbb5501961ecc34523c1905a74e7
Toshimasa Morioka <morioka.toshimasa@gmail.com>
2019-09-29 18:00:34 +0900

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyC2UDRLKXz-e9nmlRcv7c-7CT2IfiW-B_8',
+  authDomain: 'english-practice-254307.firebaseapp.com',
+  databaseURL: 'https://english-practice-254307.firebaseio.com',
+  projectId: 'english-practice-254307',
+  storageBucket: 'english-practice-254307.appspot.com',
+  messagingSenderId: '859985047413',
+  appId: '1:859985047413:web:398b80d1f9cc5c562e6d3c'
+}
